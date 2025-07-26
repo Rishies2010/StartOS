10f86f32221a3a49ade171d7c48b78583c2120d1
Rishies2010 <rishiewas2010@gmail.com>
2025-07-26 10:03:18 +0530

    wrong include fix

--- src/cpu/id/cpuid.c
+++ src/cpu/id/cpuid.c
@@ -1,6 +1,6 @@
 #include "../../libk/debug/log.h"
 #include <stdint.h>
-#include <string.h>
+#include "../../libk/string.h"
 #include "cpuid.h"
 
 static inline void cpuid(uint32_t leaf, uint32_t subleaf, uint32_t *eax, uint32_t *ebx, uint32_t *ecx, uint32_t *edx) {
