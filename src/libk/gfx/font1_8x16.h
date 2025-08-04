3b486d9ffa1419fdfe8cc132fe0dbb29b4e724ab
Rishies2010 <rishiewas2010@gmail.com>
2025-08-04 22:37:07 +0530

    font cleanup

--- src/libk/gfx/font1_8x16.h
+++
@@ -1,4 +0,0 @@
-#ifndef FONT1_8X16_H
-#define FONT1_8X16_H
-extern unsigned char font1_8x16[256][16];
-#endif
--- src/libk/gfx/font3_8x16.h
+++
@@ -1,4 +0,0 @@
-#ifndef FONT3_8X16_H
-#define FONT3_8X16_H
-extern unsigned char font3_8x16[256][16];
-#endif
